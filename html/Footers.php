a92c32d0abacd88b56bfc446433ffeb971059c5a
kiensn123 <153370385+kiensn123@users.noreply.github.com>
2024-06-11 20:44:06 +0700

    kienfoter

---
+++ html/Footers.php
@@ -0,0 +1,101 @@
+<footer>
+        <div class="tren">
+            <div>
+                <i class="bi bi-gem"></i>
+                <p>Fast Delivery</p>
+            </div>
+            <div>
+                <i class="bi bi-bookmarks"></i>
+                <p>Diverse Product Range</p>
+            </div>
+            <div>
+                <i class="bi bi-coin"></i>
+                <p>Competitive Pricing</p>
+            </div>
+            <div>
+                <i class="bi bi-star"></i>
+                <p>Quality Assurance</p>
+            </div>
+            <div>
+                <i class="bi bi-award"></i>
+                <p>Integrity-based Operation</p>
+            </div>
+        </div>
+        <div class="duoi">
+            <div class="duoi_tren">
+                <div class="trai" >
+                    <img src="/img/logo-w (1).png"/>
+                    <p>Our committed to providing high-quality and professional services to our customers, and creating a comprehensive automotive sales ecosystem.</p>
+                    <div class="icon_duoi" >
+                       
+                        <i class="bi bi-youtube"></i>
+                        <i class="bi bi-facebook"></i>
+                        <i class="bi bi-whatsapp"></i>
+                        <i class="bi bi-twitter"></i>
+                    </div>
+                </div>
+                <div class="giua" >
+                    <div>
+                        <p id="giua_tieude">All Vehicles</p>
+                        <p id="giua_tieude">Used Car</p>
+                        <p id="giua_tieude">Trucks</p>
+                    </div>
+                    <div>
+                        <p id="giua_tieude">About Us</p>
+                        <p>Brand Story</p>
+                        <p>Logistics</p>
+                        <p>Export Qualification</p>
+                        
+                    </div>
+                    <div>
+                        <p id="giua_tieude">Links</p>
+                        <p>Brand Story</p>
+                        <p>Logistics</p>
+                        <p>Export Qualification</p>
+                        
+                    </div>
+                    <div>
+                        <p id="giua_tieude">Service Policy</p>
+                        <p>Privacy Policy</p>
+                        <p>Terms of Service</p>
+                        <p>Return & Exchange</p>
+                        <p>Shipping & Tracking</p>
+                        
+                    </div>
+                </div>
+                <div class="phai" >
+                    <div>
+                        <div class="phai_icon" >
+                            <i class="bi bi-chat-dots"></i>
+                        </div>
+                        <div class="trai_icon" >
+                            <p>Phone / Whatsapp / Wechat</p>
+                            <p id="giua_tieude">+8619037179250</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="phai_icon" >
+                            <i class="bi bi-envelope-at"></i>
+                        </div>
+                        <div class="trai_icon" >
+                            <p>Send Email</p>
+                            <p id="giua_tieude">info@borderlesscar.com</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="phai_icon" >
+                            <i class="bi bi-telephone"></i>
+                        </div>
+                        <div class="trai_icon" >
+                            <p>Tel</p>
+                            <p id="giua_tieude">+86-19037179250</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="duoi"></div>
+        </div>
+
+
+
+   </footer>
--- index.php
+++ index.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php include_once("html/style.php"); ?>
 </head>
     
-    <?php include_once("html/style.php"); ?>
+  
 
 <body>
     <header>
         <?php include_once("html/Header.php"); ?>
     </header>
+
+
+
+    <?php include_once("html/Footers.php"); ?>
+
+
 </body>
 </html>
