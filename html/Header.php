951f4992c712a7b231ab082a926ccf13340d1852
HuyVu <huyhuyhuyak74@gmail.com>
2024-06-12 16:17:39 +0700

    Edit dropdown in Header.php

--- html/Header.php
+++ html/Header.php
@@ -15,33 +15,38 @@
         .navbar-brand {
             margin-right: 20px; /* Khoảng cách giữa logo và tiêu đề thương hiệu */
         }
+        .nav-item.dropdown:hover .dropdown-menu {
+          display: block;
+          margin-top: 0; /* Giúp căn chỉnh dropdown */
+        }
 </style>
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
       <img src="img\Logo-Opel.png" alt="Mô tả hình ảnh" class="img-fluid logo">
-      <a class="navbar-brand" href="#">OPEL</a>
+      <a class="navbar-brand" href="/">OPEL</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Brand</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a id="rangeOfCarLink" class="nav-link dropdown" href="\range_of_car.php" role="button">
             Range of Car
           </a>
           <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="#">PreOwned Cars</a>
         </li>
