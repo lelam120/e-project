6e745c842ce692fc319bd1b0a18f975ce8b3aa0e
laivu2407 <153612385+laivu2407@users.noreply.github.com>
2024-06-13 23:39:22 +0700

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Borderless Car</title>
-    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/about_us.css" rel="stylesheet">
+
 </head>
 
 <body>
