f103b62c566e69ad941a57188ea3b3ba303d9e30
laivu2407 <153612385+laivu2407@users.noreply.github.com>
2024-06-13 23:14:14 +0700

    about_us.php

--- about_us.php
+++ about_us.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Borderless Car</title>
-    <link href="css/about_us.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
 </head>
-
 <body>
     <header>
         <?php include_once("html/Header.php"); ?>
@@ -63,6 +61,8 @@
         <p>Welcome To Henan Borderless International Trade Co., Ltd.</p>
         <p>Our committed to providing high-quality and professional services to our customers, and creating a comprehensive automotive sales ecosystem. With over 10 years of experience and accumulated professional skills in the automotive industry, we constantly innovate and improve product quality with customer needs at the forefront, which has made us a leader in the automotive industry.</p>
         <p>Our main products include electric cars, used cars, and other well-known international brands that cover various consumer groups. Through years of market practice and technological innovation, our vehicle performance and quality have reached advanced levels both domestically and internationally. Our goal is to become a leader in the automotive sales industry, achieve mutual benefit with customers, and provide them with more professional, reliable, and comfortable services during the car-buying process.</p>
+        <!-- Hình ảnh -->
+        <img src="images/Screenshot_2024-06-13_105503.png" alt="Brand Story Image">
     </section>
 
     <!-- Professionalism Section -->
@@ -83,5 +83,4 @@
         <?php include_once("html/Footers.php"); ?>
     </footer>
 </body>
-
 </html>
