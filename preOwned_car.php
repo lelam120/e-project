2d7a1775876c8db3604cb1322649afa9acb51e36
HuyVu <huyhuyhuyak74@gmail.com>
2024-06-20 23:25:49 +0700

    ....

--- preOwned_car.php
+++ preOwned_car.php
@@ -27,7 +27,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Range Of Car</title>
+    <title>PreOwned Car</title>
     <?php include_once("html/style.php"); ?>
     <link href="css/styles.css" rel="stylesheet">
     <link href="css/trangchu.css" rel="stylesheet">
